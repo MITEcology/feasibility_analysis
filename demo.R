a072e10631bba4927c23a7df96596ad645b01063
MITEcology <33059554+MITEcology@users.noreply.github.com>
2023-08-22 12:10:26 -0400

    Update demo.R

--- demo.R
+++ demo.R
@@ -1,60 +1,33 @@
 rm(list = ls())
-source("toolbox.R")
+source("toolbox.R") ## all the main functions
 
-set.seed(2022)
-matA <- generate_inte_rand(4, 1, 1, 0, "norm")
-set.seed(2023)
-matB <- generate_inte_rand(4, 1, 1, 0, "norm")
-set.seed(2024)
-r <- matA %*% c(runif(4, 0, 1))
+### Feasibility: the size of the feasibility domain compatible with the long-term persistence of a given set of species $\mathcal{C}$ from a given community (pool) of species $\mathcal{S}$. 
+### The size is estimated by the normalized surface area of a unit sphere (i.e., also known as the normalized solid angle). 
+### Note that feasibility (size of the feasibility domain) is not directly a probability measure. 
+### To do so, one needs to normalize the feasibility such that values lie within $[0,1]$ according to the characteristics of the community (see section feasibility of a community $\mathcal{S}$). 
 
-set.seed(2025)
-matC <- generate_inte_neg_def(4, 0.8, 0.8, "norm")
-set.seed(2026)
-matD <- generate_inte_neg_def(4, 0.8, 0.8, "norm")
-# save(matA, matB, matC, matD, r, file = "testdata.RData")
+matA <- generate_inte_rand(4, 1, 1, 0, "norm")  ## Generate a random matrix
+matB <- generate_inte_neg_rand(4, 1, 1, 0, "norm") ## Generate a globally stable random matrix
+r <- matA %*% c(runif(4, 0, 1)) ## This is the direction of the r-vector (assuming it is known). This is only needed to calculate resistance and recovery
 
-feasibility_community(matC)
-# [1] 0.2682132
+feasibility_community(matA) ## If the user is interested in the species-specif measure, then the user needs to calculate raise this outcome to the power 1/|S|, where |S| is the matrix dimension  
 
-feasibility_overlap(matA, matB)
-# [1] 0.0001663809
+feasibility_overlap(matA, matB) ## Matrices need to have the same dimension
 
-feasibility_asymmetry(matA)
-# [1] 0.4558857
+feasibility_asymmetry(matA) ## This is a standard deviation. The larger the outcome, the larger the assymetry
 
-feasibility_partition(matD)
-# [1] 0.062500000 0.090493483 0.130122337 0.111260296 0.085156470 0.061411663 0.148784899 0.080082983 0.034231896 0.032469443 0.059323957 0.017098322 0.017826845
-# [14] 0.057302734 0.007891408 0.004046566
+feasibility_partition(matD) ## The feasibility of all the 2^|S| possible combinations
 
-feasibility_species(matC, sp = 2)
-# [1] 0.7020132
+feasibility_species(matC, sp = 2) ## The sum of feasibility regions whith species i
 
-feasibility_resistance_full(matA, r)
-# results might be random due to sampling of the minimum
-#      1_2_3      1_2_4      1_3_4      2_3_4
-# 0.13204785 0.26007696 0.08515493 0.25073912
+feasibility_resistance_full(matA, r) ## This is a distance from a a given r-vector to all possible |S-1| regions. The larger the outcome, the larger the resistance
 
-feasibility_resistance_partial(matA, r)
-#         1         2         3         4
-# 0.2658463 0.9230088 1.2967255 0.7911352
+feasibility_resistance_partial(matA, r) ## This is a distance from a a given r-vector to all possible vertices. The larger the outcome, the larger the resistance
 
-feasibility_recovery(matA, r, type = "full")
-#     feasibility_recovery(matA, r, type = "full")
-# [1] -1.441499
+feasibility_recovery(matA, r, type = "full") ## This is the largest eigenvalue (notice is negative)
 
-feasibility_recovery(matA, r, type = "part")
-# [1] -0.6845685
+feasibility_recovery(matA, r, type = "part") ## This is the second smallest eigenvalue (notice is negative)
 
-feasibility_contribution(matC, sp = 2)
-# 1.241339
+feasibility_contribution(matC, sp = 2) ## This is a ratio between the feasibility with |S| species and with |S-1| species. Otcomes above (resp. below) 1 mean a positive (resp. negative) contribution
 
-feasibility_invasion(matC, invader = 1)
-# $colonization
-# [1] 0.5295405
-
-# $augmentation
-# [1] 0.1123268
-
-# $replacement
-# [1] 0.4172137
+feasibility_invasion(matC, invader = 1) ## This is the feasibility region for invasion
