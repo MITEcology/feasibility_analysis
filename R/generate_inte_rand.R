4abe6f5f48a6d2c4545c7e052b9fb12803aa4197
Ignasi Bartomeus <nacho.bartomeus@gmail.com>
2023-08-27 18:38:14 +0200

    Add the first function

---
+++ R/generate_inte_rand.R
@@ -0,0 +1,33 @@
+#' Generate random interaction matrix
+#'
+#' Generate random interaction matrix A of a community S
+#'
+#' @param S Numeric, number of species
+#' @param sigma Numeric, standard deviation of interaction
+#' @param conne Numeric, connectivity of interaction, default to 1 
+#' @param mu Numeric, mean interaction, default to 0
+#' @param dist Character, distribution of interaction (normal, log-normal, Half-Normal, or Uniform). Default to "norm"
+#' 
+#' @return an SxS interaction matrix A.
+#' 
+#' @note Note that if one chooses a half-normal distribution, the interaction matrix can be interpreted as a purely competition system. As default, the diagonal values are all set to a_ii = -1 (e.g., self-regulation).
+#' 
+#' @export
+#'
+#' @examples
+#' matA <- generate_inte_rand(4, 1, 1, 0, "norm")  ## Generate a random matrix
+generate_inte_rand <- function(S, sigma, conne = 1, mu = 0, dist = "norm") {
+  #defensive programming here (i.e. check that S is numeric, dist is one of the accepted options, etc...)
+  if (dist == "norm") {
+    matA <- rnorm(S * S, mean = mu, sd = sigma)
+  } else if (dist == "lnorm") {
+    matA <- -rlnorm(S * S, mean = mu, sd = sigma)
+  }
+  zeroes <- sample(
+    c(rep.int(1, floor(S * S * conne)), rep.int(0, (S * S - floor(S * S * conne))))
+  )
+  matA[which(zeroes == 0)] <- 0
+  matA <- matrix(matA, ncol = S, nrow = S)
+  diag(matA) <- -1
+  return(matA)
+}
