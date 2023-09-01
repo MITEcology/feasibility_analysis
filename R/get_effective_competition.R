1867947d7b1fd3bd5838dbaffc0b5a0123ed49a9
Chengyi Long <79570794+cy-long@users.noreply.github.com>
2023-09-01 17:07:10 -0400

    Update effective_competition

--- R/get_effective_competition.R
+++ R/get_effective_competition.R
@@ -1,18 +1,18 @@
 #' Get effective competition matrix
 #' @description TBA
-#' 
-#' @param A matrix of bipartite network (?)
-#' 
+#'
+#' @param matA matrix of bipartite network (?)
+#'
 #' @return effective competition matrix
-#' 
+#'
 #' @examples
-#' TBA
+#' # TBA
 #'
 #' @export
-get_effective_competition <- function(A){
-  gram <- t(A) %*% A
-  for(i in 1:ncol(gram)){
-    gram[,i] <- gram[,i]/sum(gram[,i])
+get_effective_competition <- function(matA) {
+  gram <- t(matA) %*% matA
+  for (i in seq_len(ncol(gram))) {
+    gram[, i] <- gram[, i] / sum(gram[, i])
   }
   diag(gram) <- 1
   return(gram)
