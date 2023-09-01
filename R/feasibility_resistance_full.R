1236120717226804b5c7e4d2e0e2c2d14b7a6adc
MITEcology <33059554+MITEcology@users.noreply.github.com>
2023-09-01 16:12:02 -0400

    Update feasibility_resistance_full.R

--- R/feasibility_resistance_full.R
+++ R/feasibility_resistance_full.R
@@ -1,6 +1,6 @@
 #' Calculate the full resistance to parameter perturbations
 #'
-#' @description full resistance of community S to parameter perturbations
+#' @description full resistance (all species remain extanct) of a community to parameter perturbations. Full resistance is the distance between an intrinsic growth rate vector inside the feasibility domain and a border of the domain. It can be calculated by the nearest arc length (i.e. L2 norm) from \eqn{r} to a border as: \eqn{d_b = \arccos <r(N^*), r(\mathrm{border})>}.
 #'
 #' @param matA the interaction matrix
 #' @param r the intrinsic growth rate
@@ -8,18 +8,17 @@
 #' @param nt the number of sampling points on the border of feasibility domain
 #' @param all whether to return all resistance values from the samples or taking the minimal value as the estimated resistance
 #'
-#' @note full resistance is the distance between an intrinsic growth rate vector inside the feasibility domain and a border of \eqn{D(\mathcal{S})}. It can be calculated by the nearest arc length (i.e. L2 norm) from \eqn{r} to a border as: \eqn{d_b = \arccos <r(N^*), r(\mathrm{border})>}; partial resistance is similarly defined as distance between the intrinsic growth rate vector to the vertices of feasibility domain.
 #'
 #' @importFrom magrittr %>%
 #' @importFrom purrr map_dbl
 #' @importFrom utils combn
 #'
-#' @return a numeric value of the full resistance
+#' @return a numeric value of the full resistance without having species going exrtinct.
 #'
 #' @examples
-#' matA <- generate_inte_rand(4, 1, 1, "norm")
-#' r <- matA %*% c(runif(4, 0, 1))
-#' feasibility_resistance_full(matA, r)
+#' matA <- generate_inte_rand(4, 1, 1, "norm") ## Generate random interaction matrix with nA=4 species
+#' r <- matA %*% c(runif(4, 0, 1)) ## Generate random location inside the feasibility domain
+#' feasibility_resistance_full(matA, r) ## Extent of parameter perturbations without leading to species extinctions.
 #' @export
 #' @seealso feasibility_resistance_partial
 feasibility_resistance_full <- function(matA, r, norm = "l2", nt = 100, all = FALSE) {
