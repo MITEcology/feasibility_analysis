86ccb81d235d24136f11446b6df0998101679f8d
MITEcology <33059554+MITEcology@users.noreply.github.com>
2023-09-01 15:54:50 -0400

    Update feasibility_contribution.R

--- R/feasibility_contribution.R
+++ R/feasibility_contribution.R
@@ -1,20 +1,20 @@
-#' Calculate contribution to the feasibility of a community by a species
+#' Calculate contribution to the feasibility of a community S by a species i
 #'
-#' @description The ratio between the feasibility with \eqn{S} species and with \eqn{S-1} species.
-#' @param matA Numeric, an \eqn{S\times S} interaction matrix \eqn{A}
+#' @description The ratio between the feasibility with n species and with n-1 species. How much the feasibility of a community S is positively or negatively affected by a species i. Outcomes above (resp. below) 1 mean a positive (resp. negative) contribution.
+#' @param matA Numeric, a nxn interaction matrix A
 #'
 #' @param sp Numeric, index of the focal species
 #' @param nt Numeric, number of replications to reduce numerical instabilities, default to 30
 #'
-#' @return How much the feasibility of a community S is positively or negatively affected by a species i. Outcomes above (resp. below) 1 mean a positive (resp. negative) contribution.
+#' @return Contribution of species i to the feasibility of a community
 #'
 #' @importFrom mvtnorm pmvnorm
 #'
 #' @export
 #'
 #' @examples
-#' matA <- generate_inte_gs(4, 1, 1, "norm")  ## Generate a random interaction matrix
-#' feasibility_contribution(matA, sp = 1)
+#' matA <- generate_inte_gs(4, 1, 1, "norm")  ## Generate a random interaction matrix of nA=4 species
+#' feasibility_contribution(matA, sp = 1) ## Contribution of species 1 to the feasibiltiy of the entire community of nA=4 species.
 feasibility_contribution <- function(matA, sp, nt = 30) {
   S <- ncol(matA)
 
