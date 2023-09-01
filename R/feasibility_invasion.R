109d086089bc7f2e5b6051685f8308fe4fda0f64
MITEcology <33059554+MITEcology@users.noreply.github.com>
2023-09-01 15:59:46 -0400

    Update feasibility_invasion.R

--- R/feasibility_invasion.R
+++ R/feasibility_invasion.R
@@ -1,10 +1,11 @@
-#' Calculate feasibility of invasion to a community by a species
+#' Calculate feasibility of invasion to a community by a species i
 #'
-#' @param matA Numeric, an SxS interaction matrix A
+#' @description Estimates the feasibility that a species i can invade a community of n-1 species. Feasibility of the domain of positive intrinsic growth rate (denoted by IGR_i>0). This considers that the rest of the species (\eqn{j \neq i \in \mathcal{S}}) are already at equilibrium. Recall that this can be transformed into a probability.
+#' @param matA Numeric, a nxn interaction matrix A
 #' @param inv Numeric, index of the invading species
 #' @param nt Numeric, number of sampling points, default to 5000
 #'
-#' @return Feasibility of the domain of positive intrinsic growth rate (denoted by \eqn{IGR_i>0}). This considers that the rest of the species (\eqn{j \neq i \in \mathcal{S}}) are already at equilibrium.
+#' @return Feasibility of invasion of species i to a community of n-1 species
 #'
 #' @importFrom magrittr %>%
 #' @importFrom purrr map_dbl
@@ -12,8 +13,8 @@
 #' @export
 #'
 #' @examples
-#' matA <- generate_inte_gs(4, 1, 1, "norm")  ## Generate a random interaction matrix
-#' feasibility_invasion(matA, inv = 1)
+#' matA <- generate_inte_gs(4, 1, 1, "norm")  ## Generate a random interaction matrix of nA=4 species
+#' feasibility_invasion(matA, inv = 1) ## Estimates the feasibility of invasion by species 1 to the nA-1 species.
 feasibility_invasion <- function(matA, inv, nt = 5000) {
   S <- nrow(matA)
   res <- c(1:S)[-inv]
