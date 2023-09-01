4e52f48a44543a183f1e7e105fbc91bec6a1b4df
MITEcology <33059554+MITEcology@users.noreply.github.com>
2023-09-01 16:08:26 -0400

    Update feasibility_recovery.R

--- R/feasibility_recovery.R
+++ R/feasibility_recovery.R
@@ -1,20 +1,18 @@
-#' Calculate the recovery to abundance perturbations
+#' Calculate the recovery of a community to abundance perturbations
 #'
-#' @description recovery rate of community \eqn{\mathcal{S}} to abundance perturbations
+#' @description recovery rate of community with n species to random abundance perturbations. Recovery rate is defined as the real parts of eigenvalues of the Jacobian matrix \eqn{\mathbf{A}\mathrm{diag}(N^{*})}, the largest eigenvalue \eqn{\lambda_1} is full recovery rate, the second smallest eigenvalue \eqn{\lambda_{S-1}} is partial recovery rate. (largest and smallest in terms of absolute values)
 #'
 #' @param matA the interaction matrix
 #' @param r the intrinsic growth rate
 #' @param type the type of recovery rate, either "full" or "part"
 #'
-#' @note recovery rate is defined as the real parts of eigenvalues of the Jacobian matrix \eqn{\mathbf{A}\mathrm{diag}(N^{*})}, the largest eigenvalue \eqn{\lambda_1} is full recovery rate, the second smallest eigenvalue \eqn{\lambda_{S-1}} is partial recovery rate. (largest and smallest in terms of absolute values)
-#'
-#' @return the recovery rate with corresponding type
+#' @return the recovery rate to having all species (full) or a subset of species (part)
 #'
 #' @examples
-#' matA <- generate_inte_rand(4,1,1,"norm")
-#' r <- matA %*% c(1,2,1,1)
-#' feasibility_recovery(matA, r, "full")
-#' feasibility_recovery(matA, r, "part")
+#' matA <- generate_inte_rand(4,1,1,"norm") ## Generate random interaction matrix with nA=4 species
+#' rA <- matA %*% c(1,2,1,1) ## Generate a random location inside the feasibility domain.
+#' feasibility_recovery(matA, rA, "full")
+#' feasibility_recovery(matA, rA, "part")
 #'
 #' @export
 feasibility_recovery <- function(matA, r, type) {
