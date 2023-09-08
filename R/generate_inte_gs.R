ab6bc937ff276a52d8b15d36fdee6f879bd558b9
Chengyi Long <79570794+cy-long@users.noreply.github.com>
2023-09-08 14:16:42 -0400

    Update generate_inte_gs

--- R/generate_inte_gs.R
+++ R/generate_inte_gs.R
@@ -1,41 +1,39 @@
 #' Generate random interaction matrix (globally stable)
 #'
-#' @description Generate a globally stable random interaction matrix A of a community \eqn{\mathcal{S}}
+#' @description Generate globally stable random interaction matrices A of a community \eqn{\mathcal{S}}
 #'
 #' @param S Numeric, number of species
 #' @param sigma Numeric, standard deviation of interaction
 #' @param conne Numeric, connectivity of interaction, default to 1
 #' @param dist Character, distribution of interaction, default to "norm"
 #' @param mu Numeric, mean interaction, default to 0
-#' @param trail Numeric, starting number of trails to generate negative definite matrix, default to 0
+#' @param rep Numeric, number of matrices wanted, default to 1
 #'
-#' @return a globally stable SxS interaction matrix A.
+#' @return a single matrix or a list of globally stable SxS interaction matrices A.
 #'
-#' @note Note that if one chooses a half-normal distribution, the interaction matrix can be interpreted as a purely competition system. As default, the diagonal values are all set to a_ii = -1 (e.g., self-regulation).
+#' @note Note that if one chooses a half-normal distribution, the interaction matrix can be interpreted as a purely competition system.
+#'
+#' As default, the diagonal values are all set to a_ii = -1 (e.g., self-regulation).
 #'
 #' @examples
 #' matA <- generate_inte_gs(4, 1, 1, "norm")  ## Generate a globally stable random interaction matrix
 #' matA
-#' 
+#'
 #' @export
-generate_inte_gs <- function(S, sigma, conne = 1, dist = "norm", mu = -0.1 * sigma, trail = 0) {
-  if (dist == "norm") {
-    matA <- rnorm(S * S, mean = mu, sd = sigma)
-  } else if (dist == "lnorm") {
-    matA <- -rlnorm(S * S, mean = mu, sd = sigma)
+generate_inte_gs <- function(S, sigma, conne = 1, dist = "norm", mu = 0, rep = 1) {
+  i <- 1; j <- 1
+  list_matA <- list()
+
+  while (i <= 10000 * rep) {
+    matA <- generate_inte_rand(S, sigma, conne, dist, mu)
+    if (check_negative_definite(matA)) {
+      list_matA[[j]] <- matA
+      j <- j + 1
+    }
+    if (j == rep + 1) break
+    i <-  i + 1
+    if (i == 10000 * rep) stop("Cannot generate a globally stable matrix")
   }
-  zeroes <- sample(
-    c(rep.int(1, floor(S * S * conne)), rep.int(0, (S * S - floor(S * S * conne))))
-  )
-  matA[which(zeroes == 0)] <- 0
-  matA <- matrix(matA, ncol = S, nrow = S)
-  diag(matA) <- -1
-  if (check_negative_definite(matA)) {
-    return(matA)
-  } else if (trail < 100) {
-    trail <- trail + 1
-    generate_inte_gs(S, sigma, conne, dist, mu, trail)
-  } else {
-    stop("Error: cannot generate negative definite matrix within 100 trails")
-  }
+  if (rep == 1) return(list_matA[[1]])
+  else return(list_matA)
 }
